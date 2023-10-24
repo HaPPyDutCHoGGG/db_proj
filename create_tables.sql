eff50e253ed5d40f2000a88ea8ac8d2aba46464a
HaPPyDutCHoGGG <hdh.egor.shermatov@gmail.com>
2023-10-25 01:08:03 +0300

    read_me

--- create_tables.sql
+++ create_tables.sql
@@ -1,32 +1,45 @@
-create table city
+create table address
 (
-	id_city serial primary key, 
+	id_address serial primary key, 
 	city_name varchar unique not null,
-	city_country varchar not null	
+	country_name varchar not null,
+	actual_address varchar not null,
+	world_region varchar not null
 );
+/*changed table city to table address*/
 
 create table client
 (
 	id_client serial primary key, 
 	client_name varchar not null,
 	client_email varchar unique not null,
-	id_city int references city(id_city)
+	id_address int references address(id_address)
 );
 
 create table producer
 (
 	id_producer serial primary key, 
 	producer_name varchar not null,
-	producer_contacts varchar unique not null,
-	id_city int references city(id_city)
+	producer_contact varchar unique not null,
+	producer_site varchar unique not null,
+	producer_email varchar unique not null,
+	id_address int references address(id_address)
 );
-/**/
+
+create table stock_producer
+(
+	id_stock serial primary key,
+	id_owner int references producer(id_producer),
+	id_address int references address(id_address)
+);
+
+/*done--*/
 
 create table category
 (
 	id_category serial primary key,
 	category_name varchar unique not null
-)
+);
 
 create table product 
 (
@@ -36,38 +49,61 @@ create table product
 	fragility bool not null,
 	id_category int references category(id_category),
 	id_producer int references producer(id_producer)
-)
+);
+
+create table availability_status_producer
+(
+	id_stock int references stock_producer(id_stock),
+	id_product int references product(id_product), 
+	availability bool not null
+);
 
 create table orders
 (
+	id_order serial primary key, 
 	id_client int references client(id_client),
 	id_product int references product(id_product),
-	production_date varchar unique not null,
-	constraint orders_pk primary key (id_client,id_product)
-)
-/**/
+	production_date varchar unique not null
+);
+/*done--*/
 
 create table supplier 
 (
 	id_supplier serial primary key,
 	supplier_name varchar unique not null,
-	supplier_email varchar unique not null
-)
-***
+	supplier_contact varchar unique not null,
+	supplier_email varchar unique not null,
+	id_address int references address(id_address)	
+);
+
+create table stock_supplier
+(
+	id_stock serial primary key,
+	id_owner int references supplier(id_supplier),
+	id_address int references address(id_address)
+);
 
 create table material 
 (
 	id_material serial primary key,
 	material_name varchar unique not null,
 	material_price decimal not null,
-	material_weight decimal not null,
-	delivery_number int not null,
+	weight_of_unit decimal not null,
+	units_in_bunch int not null,
 	id_supplier int references supplier(id_supplier)
-)
-/*mb should make delivery_number uniquee*/
+);
+
+create table availability_status_supplier
+(
+	id_stock int references stock_supplier(id_stock),
+	id_product int references material(id_material), 
+	availability bool not null
+);
+/*done--*/
 
 create table compound
 (
 	id_product int references product(id_product),
 	id_material int references material(id_material)
-)
+);
+
---
+++ read_me.txt
@@ -0,0 +1,4 @@
+changes in db(market):
+1) changed table city to table address (need to routes on map in the future);
+2) added stocks for producers and suppliers (id, owner, address);
+3) products and materials now have availability_status;
